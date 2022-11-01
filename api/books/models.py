92b2bd27a501b6e0e30f650ab2963bd82f236c77
DenverJensen <jensendenver@gmail.com>
2022-11-01 13:14:28 -0600

    update to books

---
+++ api/books/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class BookData(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=200)
+    duration = models.FloatField()
+    rating = models.FloatField()
+    genre = models.CharField(max_length=200, default='favorites')
+    image = models.ImageField(upload_to='images', default='images/none/noimg.jpg')
